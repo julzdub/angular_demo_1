5510c8d4a008a5ec88b2165665339c4586796fcd
Julian Welge <julianwelge@gmail.com>
2022-08-05 11:57:43 -0700

    google map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
